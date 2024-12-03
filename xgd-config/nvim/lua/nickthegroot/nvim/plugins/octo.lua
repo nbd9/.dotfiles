ed5797baa79aec92b96c4d85f16cc8757f2dd843
Nick DeGroot <nbdegroot1@gmail.com>
2024-12-02 16:02:29 -0800

    20241202 Update (#3)
    Co-authored-by: Nick DeGroot <1966472+nickthegroot@users.noreply.github.com>

---
+++ xgd-config/nvim/lua/nickthegroot/nvim/plugins/octo.lua
@@ -0,0 +1,14 @@
+return {
+  'pwntester/octo.nvim',
+  cmd = 'Octo',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('octo').setup {
+      use_local_fs = true,
+    }
+  end,
+}
--- xgd-config/nvim/lua/nickthegroot/nvim/plugins/telescope.lua
+++ xgd-config/nvim/lua/nickthegroot/nvim/plugins/telescope.lua
@@ -45,15 +45,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
     -- [[ Configure Telescope ]]
     -- See `:help telescope` and `:help telescope.setup()`
     require('telescope').setup {
-      -- You can put your default mappings / updates / etc. in here
-      --  All the info you're looking for is in `:help telescope.setup()`
-      --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
-      -- pickers = {}
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
@@ -69,7 +60,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sf', builtin.git_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
